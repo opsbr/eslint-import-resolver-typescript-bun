779daa9c42d03856c90cfff142c3dcea6e8c64f1
Ryosuke Iwanaga <riywo@opsbr.com>
2023-07-02 01:35:19 +0000

    chore: update repository data and readme

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,6 +2,10 @@ import { OpsBRTypeScriptProject } from "@opsbr/projen-typescript";
 const project = new OpsBRTypeScriptProject({
   name: "eslint-import-resolver-typescript-bun",
   defaultReleaseBranch: "main",
+  repository: "https://github.com/opsbr/eslint-import-resolver-typescript-bun",
+  homepage: "https://github.com/opsbr/eslint-import-resolver-typescript-bun",
+  bugsUrl:
+    "https://github.com/opsbr/eslint-import-resolver-typescript-bun/issues",
   deps: ["debug", "eslint-import-resolver-typescript"],
   devDeps: ["@opsbr/projen-typescript", "@types/debug"],
   peerDeps: ["bun-types"],
